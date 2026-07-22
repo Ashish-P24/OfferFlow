ed2da032a89a399c097d35fe27e86ea30b21f283
Ashish-P24 <ashishvpathak24@gmail.com>
2026-07-22 19:45:08 +0530

    chore: initialize Spring Boot backend and PostgreSQL configuration

---
+++ backend/src/main/java/com/offerflow/OfferflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.offerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferflowApiApplication.class, args);
+	}
+
+}
